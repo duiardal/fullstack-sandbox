d5e53efd2c4a1af1b43d292035e0fe99b6e33696
Dui Ardal <dui@Duis-MBP.lan>
2020-03-31 22:51:59 +0200

    cleanup: remove comments and unused code

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,163 +1,6 @@
-// const apiEndpoint = "http://localhost:3001/api/todoList";
-
-// export const getTodos = async () => {
-//   const response = await fetch(apiEndpoint);
-//   const body = await response.json();
-//   if (response.status !== 200) throw Error(body.message);
-//   return body;
-// };
-
-// export const initialState = getTodos();
-
-// export const reducer = async (state, action) => {
-//   switch (action.type) {
-//     case "add": {
-//       const response = await fetch(apiEndpoint, {
-//         method: "POST",
-//         headers: {
-//           "Content-Type": "application/json"
-//         },
-//         body: JSON.stringify({
-//           id: state,
-//           title: state,
-//           todos: []
-//         })
-//       });
-
-//       const body = await response.json();
-
-//       console.log(state, body)
-
-//       const newTodo = {
-//         id: body.id,
-//         title: body.title,
-//         todos: body.todos
-//       };
-
-//       console.log(newTodo)
-//       return {
-//         todos: [...state.todos, newTodo]
-//       };
-//     }
-//     case "edit": {
-//       const idx = state.todos.findIndex(t => t.id === action.id);
-//       const todo = Object.assign({}, state.todos[idx]);
-//       todo.text = action.text;
-//       const todos = Object.assign([], state.todos);
-//       todos.splice(idx, 1, todo);
-//       return {
-//         counter: state.counter,
-//         todos: todos
-//       };
-//     }
-//     case "remove": {
-//       const idx = state.todos.findIndex(t => t.id === action.id);
-//       const todos = Object.assign([], state.todos);
-//       todos.splice(idx, 1);
-//       return {
-//         counter: state.counter,
-//         todos: todos
-//       };
-//     }
-//     default:
-//       return state;
-//   }
-// };
-
-// actions
-// const INITIAL = "INITIAL";
-// const TOGGLE_COMPLETE = "TOGGLE_COMPLETE";
-// const ADD = "ADD";
-// const DELETE = "DELETE";
-// const SET_ACTIVE_LIST = "SET_ACTIVE_LIST";
-// export const COMP_ALL = "COMP_ALL";
-
-// // action creators
-// export const toggleComplete = id => ({
-//   type: TOGGLE_COMPLETE,
-//   id
-// });
-
-// export const fetchInitial = data => ({
-//   type: INITIAL,
-//   data
-// });
-
-// export const addTodo = data => {
-//   if (!data) return;
-
-//   return {
-//     type: ADD,
-//     data
-//   };
-// };
-
-// export const removeTodo = data => {
-//     return {
-//       type: DELETE,
-//       data
-//     };
-// };
-
-// export const setActiveList = data => {
-//   console.log(data)
-//   return {
-//     type: SET_ACTIVE_LIST,
-//     data
-//   }
-// };
-
-// export const reducer = (state, action) => {
-//   switch (action.type) {
-//     case INITIAL:
-//       return [...action.data];
-
-//     case TOGGLE_COMPLETE:
-//       return state.map(todo => {
-//         if (todo.id === action.id) {
-//           return { ...todo, completed: !todo.completed };
-//         }
-//         return todo;
-//       });
-
-//     case COMP_ALL:
-//       return state.map(todo => {
-//         if (todo.completed === false) {
-//           return { ...todo, completed: true };
-//         }
-//         return todo;
-//       });
-
-//     case ADD:
-//       return [action.data, ...state];
-
-//     case DELETE:
-
-//       const index = state.findIndex(t => t.id === action.data.id);
-//       const todos = Object.assign([], state);
-//       todos.splice(index, 1);
-//       console.log(action.data)
-//       return [...todos];
-
-//     case SET_ACTIVE_LIST:
-//       console.log(state);
-//       return state;
-
-//     default:
-//       return state;
-//   }
-// };
-
-
-// actions
-const INITIAL = "INITIAL";
-const ADD = "ADD";
-const REMOVE = "REMOVE";
-const EDIT = "EDIT";
-
 export const fetchInitial = data => {
   return {
-    type: INITIAL,
+    type: "INITIAL",
     data
   }
 };
@@ -166,7 +9,7 @@ export const addTodo = data => {
   if (!data) return;
 
   return {
-    type: ADD,
+    type: "ADD",
     data
   };
 };
@@ -174,14 +17,14 @@ export const addTodo = data => {
 export const editTodo = data => {
   console.log(data);
   return {
-    type: EDIT,
+    type: "EDIT",
     data
   };
 };
 
 export const removeTodo = data => {
     return {
-      type: REMOVE,
+      type: "REMOVE",
       data
     };
 };
@@ -189,22 +32,22 @@ export const removeTodo = data => {
 export const reducer = (state, action) => {
   switch (action.type) {
 
-    case INITIAL:
+    case "INITIAL":
       {
         return [...action.data];
       }
-    case ADD:
+    case "ADD":
       {
         return [...state, action.data];
       }
-    case EDIT:
+    case "EDIT":
       {
         const index = state.findIndex(t => t.id === action.data.id);
         const todos = Object.assign([], state);
         todos[index] = action.data;
         return todos;
       }
-    case REMOVE:
+    case "REMOVE":
       {
         const index = state.findIndex(t => t.id === action.data.id);
         const todos = Object.assign([], state);
